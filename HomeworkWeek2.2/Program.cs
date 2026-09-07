29a361f966b6d882aaa06e747af550098bc81ab7
MacAttack824 <cmaclelland01@gmail.com>
2026-09-07 01:41:00 -0400

    migrated HomeworkWeek2 assignment to original repo

---
+++ HomeworkWeek2.2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace HomeworkWeek2._2
+{
+    using static System.Console;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare variable for patient name
+            string patientName;
+            // program begins with appointment of patient
+            WriteLine("who is being seen today");
+            // ask user for patient name
+            patientName = ReadLine();
+            // greet patient
+            WriteLine("welcome: " + patientName);
+
+
+        }
+    }
+}
