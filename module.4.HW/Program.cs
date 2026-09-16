6d76a4b10729488fcecc75568ffd237a370af874
MacAttack824 <cmaclelland01@gmail.com>
2026-09-16 19:18:08 -0400

    start assignment with comments

--- module.4.HW/Program.cs
+++ module.4.HW/Program.cs
@@ -1,11 +1,43 @@
 ﻿namespace module._4.HW
 {
+    using System.Runtime.Intrinsics.X86;
     using static System.Console;
     class Program
     {
         static void Main(string[] args)
         {
             WriteLine("Hello, World!");
+            ReadLine();
+            // 1. Create a menu to include
+            // a.Doctor – Sick Appointment
+            // b.Doctor – Check - up
+
+
+
+
+            // 2.You will use a switch statement and have a case for each of the above.
+
+
+
+
+
+            // 3.Within Sick appointment(use if/ ese statements)
+            // a.Ask if child or adult(base visit child = $50, adult = 75)
+            // b.Did the user have labs done(+$25)
+            // c.Show out the total for the appointment and format to currency.
+
+
+
+
+
+            // 4.Within Check - up(use if/else statement)
+            // a.Ask if child or adult(child $75 and adult = 100)
+            // b.Show out the charge
+
+
+
+
+            // Note: Be sure to format the output to currency!
 
 
 
@@ -19,6 +51,13 @@
 
 
 
+
+
+
+
+
+
+            ReadKey();
         }
     }
 }
