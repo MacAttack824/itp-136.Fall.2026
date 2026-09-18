18c7641c6a4028a8b22abbd333ff73870fa3ea26
MacAttack824 <cmaclelland01@gmail.com>
2026-09-18 00:35:11 -0400

    Finished assignment HW 4

--- module.4.HW/Program.cs
+++ module.4.HW/Program.cs
@@ -1,63 +1,109 @@
 ﻿namespace module._4.HW
 {
-    using System.Runtime.Intrinsics.X86;
+    
     using static System.Console;
     class Program
     {
         static void Main(string[] args)
         {
-            WriteLine("Hello, World!");
-            ReadLine();
+            string option, age;
+            int sickCost, checkUpCost, wellCost;
+            string labs = null;
             // 1. Create a menu to include
             // a.Doctor – Sick Appointment
             // b.Doctor – Check - up
 
+            WriteLine("Welcome to Mac's Medical, are you here for a sick appointment or a well check-up? Please enter 'a' for Sick Appointment or 'b' for Check - up");
 
-
+            option = (ReadLine());
 
             // 2.You will use a switch statement and have a case for each of the above.
 
+            switch(option)
+            {
+                case "a":
+                    WriteLine("You have selected a sick appointment.");
+
+                    WriteLine("Is the patient being seen today a child or an adult?");
+                    age = ReadLine();
+
+                    // a.Ask if child or adult(base visit child = $50, adult = 75)
+
+                    // 3.Within Sick appointment(use if/ ese statements)
+
+                    if (age == "child")
+                    {
+                        WriteLine("Base charge for a child is $50");
+                        sickCost = 50;
+                        
+                        // b.Did the user have labs done(+$25)
+
+                        WriteLine("Did the patient have any labs done today? Please enter 'yes' or 'no'.");
+                        labs = ReadLine();
+
+                        if (labs == "yes")
+                        {
+                            WriteLine("Labs were done today and there will be an additional charge of $25");
+                            sickCost += 25;
+                        }
+                        WriteLine("The total cost for the sick appointment is {0:C}", sickCost);
+                    }
+
+                    else if (age == "adult")
+                    {
+                        WriteLine("Base charge for an adult is $75");
+                        sickCost = 75;
+
+                        // b.Did the user have labs done(+$25)
+
+                        WriteLine("Did the patient have any labs done today? Please enter 'yes' or 'no'.");
+                        labs = ReadLine();
+
+                        if (labs == "yes")
+                        {
+                            WriteLine("Labs were done today and there will be an additional charge of $25");
+                            sickCost += 25;
+                        }
+                    WriteLine("The total cost for the sick appointment is {0:C}", sickCost);
+                    }
+                    // c.Show out the total for the appointment and format to currency.
+
+                    break;
+
+                case "b":
+                    WriteLine("You have selected a well check-up.");
+
+                    WriteLine("Is the patient being seen today a child or an adult?");
+                    age = ReadLine();
+
+                    // 4.Within Check - up(use if/else statement)
+
+                    if (age == "child")
+                    {
+                        WriteLine("The charge for a child is $75");
+                        wellCost = 75;
+               
+                        WriteLine("The total cost for the well visit today is {0:C}", wellCost);
+                    }
+
+                    else if (age == "adult")
+                    {
+                        WriteLine("The charge for an adult is $100");
+                        wellCost = 100;
+                        
+                        WriteLine("The total cost for the well visit today is {0:C}", wellCost);
+                    }
+
+                    // a.Ask if child or adult(child $75 and adult = 100)
+                    // b.Show out the charge
 
 
-
-
-            // 3.Within Sick appointment(use if/ ese statements)
-            // a.Ask if child or adult(base visit child = $50, adult = 75)
-            // b.Did the user have labs done(+$25)
-            // c.Show out the total for the appointment and format to currency.
-
-
-
-
-
-            // 4.Within Check - up(use if/else statement)
-            // a.Ask if child or adult(child $75 and adult = 100)
-            // b.Show out the charge
-
-
-
-
-            // Note: Be sure to format the output to currency!
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            ReadKey();
+                    break;
+                
+                default:
+                    WriteLine("You have not selected a valid option.");
+                    break;
+            }
         }
     }
 }
